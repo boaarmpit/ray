ad667a4ee236a8ea9ff81051d690f2718d86c79e
Bryn Baritompa <baritompa@gmail.com>
2014-11-06 00:25:12 +0900

    Hacked together sphere reflecting checkered plane. -> Now shows the plane and basic shading.      Requires restructuring.  300ms / 500x500px.

--- src/ray/core.clj
+++ src/ray/core.clj
@@ -29,8 +29,8 @@
   (def res-x 500)
   (def res-y 500))
 
-;setup camera
-(def camera-location (array [0 1 0]))
+;;setup camera
+;(def camera-location (array [0 1 0]))
 
 (defn camera-ray
   "x and y are coordinates from 0 to 1, starting at the top left of the image"
@@ -46,7 +46,7 @@
 (defn checker-texture [x y checker-spacing]
   (let [xs (math/round (/ (mod x checker-spacing) checker-spacing))
         ys (math/round (/ (mod y checker-spacing) checker-spacing))]
-    (mod (+ xs ys) 2.0)))
+    (* 0.7 (mod (+ xs ys) 2.0))))
 
 (defn ray-plane
   "Ray-(ground)Plane Intersection:
@@ -65,7 +65,6 @@
    The ray is defined as P0+tP1 with parameter t
    The sphere is centered at C with radius r"
   [P0 P1 C r]
-  (v/normalise! P0)
   (v/normalise! P1)
   (let [a (v/dot P1 P1)
         b (* 2 (v/dot P1 (- P0 C)))
@@ -90,6 +89,7 @@
 
 (defn render-sphere []
   (let [colour-result (v/vec4 [0 0 0 1])
+        ^Vector3 camera-location (v/vec3 [0 1 0])
         ^Vector3 camera-direction (v/vec3 [0 0 1])
         ^Vector3 camera-x (v/vec3 [1 0 0])
         ^Vector3 camera-y (v/vec3 [0 1 0])
@@ -117,10 +117,12 @@
 
 (defn render-reflective-sphere []
   (let [colour-result (v/vec4 [0 0 0 1])
-        ^Vector3 camera-direction (v/vec3 [0 0 1])
-        ^Vector3 camera-x (v/vec3 [1 0 0])
-        ^Vector3 camera-y (v/vec3 [0 1 0])
-        ^Vector3 sphere-origin (v/vec3 [0 1 3])
+        ^Vector3 camera-location (v/vec3 [0 1.3 0])
+        ^Vector3 camera-direction (v/vec3 [1 0 1])
+        ^Vector3 camera-x (v/vec3 [0.5 0 -0.5])
+        ^Vector3 camera-y (v/vec3 [0 0.707 0])
+        ^Vector3 sphere-origin (v/vec3 [3 1 3])
+        ^Vector3 light-direction (v/normalise (v/vec3 [-1 1 -2]))
         sphere-radius 1
 
         ;MUTABLES
@@ -130,33 +132,55 @@
     (dotimes [ix res-x]
       (dotimes [iy res-y]
         (.set dir camera-direction)
-        (v/add-multiple! dir camera-x (* 2 (- (/ (double ix) res-x) 0.5)))
-        (v/add-multiple! dir camera-y (* -2 (- (/ (double iy) res-y) 0.5)))
+        (v/add-multiple! dir camera-x (* 2 (- (/ (+ (* 1 (rand)) (double ix)) res-x) 0.5)))
+        (v/add-multiple! dir camera-y (* -2 (- (/ (+ (* 1 (rand)) (double iy)) res-y) 0.5)))
         (v/normalise! dir)
-        (let [{:keys [sphere-intersection normal]} (ray-sphere camera-location dir sphere-origin sphere-radius)]
+        (let [{:keys [sphere-intersection normal]} (ray-sphere camera-location dir sphere-origin sphere-radius)
+              plane-intersection (ray-plane camera-location dir)]
           (if sphere-intersection
-            (let [plane-intersection (ray-plane sphere-intersection normal)]
-              (let [pixel-intensity (if plane-intersection
-                                      (let [u-tex (mget plane-intersection 0)
-                                            v-tex (mget plane-intersection 2)
-                                            pixel-intensity (+ (checker-texture u-tex v-tex 1) (/ (math/expt (- (v/mget normal 2)) 30) 1.5) (/ (- (v/mget normal 2)) 3))]
-                                        pixel-intensity
-                                        )
-                                      (+  (/ (math/expt (- (v/mget normal 2)) 0) 1.5) (/ (- (v/mget normal 2)) 3)))]
+            (let [plane-intersection2 (ray-plane sphere-intersection normal)]
+              (let [pixel-intensity (if plane-intersection2
+                                      (let [u-tex (mget plane-intersection2 0)
+                                            v-tex (mget plane-intersection2 2)
+                                            pixel-intensity (* (* 1 (checker-texture u-tex v-tex 1))
+                                                               (+ (* 0.5 (math/expt (Math/sin (* pi 0.5 (dot normal
+                                                                                                           (v/vec3
+                                                                                                               light-direction)))) 50))
+                                                                  (* 0.3 (math/expt (Math/sin (* pi 0.5 (dot normal
+                                                                                                           (v/vec3
+                                                                                                               light-direction)))) 5))
+                                                                  ))]
+                                        pixel-intensity)
+                                      (+ (* 0.5 (math/expt (Math/sin (* pi 0.5 (dot normal (v/vec3 light-direction)))
+                                                                     ) 50))
+                                         (* 0.3 (math/expt (Math/sin (* pi 0.5 (dot normal (v/vec3 light-direction)))
+                                                                     ) 5))))]
                 (.setValues colour-result ^double pixel-intensity ^double pixel-intensity ^double pixel-intensity 1.0)
-                (.setRGB im ix iy (c/argb-from-vector4 colour-result))))))))
+                (.setRGB im ix iy (c/argb-from-vector4 colour-result))))
+            (if plane-intersection
+              (let [u-tex (mget plane-intersection 0)
+                    v-tex (mget plane-intersection 2)
+                    pixel-intensity (checker-texture u-tex v-tex 2)
+                    ]
+                (.setValues colour-result ^double pixel-intensity ^double pixel-intensity ^double pixel-intensity 1.0)
+                (.setRGB im ix iy (c/argb-from-vector4 colour-result)))
+              (let [pixel-intensity 0.0]
+                (.setValues colour-result pixel-intensity pixel-intensity pixel-intensity 1.0)
+                (.setRGB im ix iy (c/argb-from-vector4 colour-result))
+                ))))))
     im))
 
 
 
 ;(time (image/show (render-plane-and-sphere) :zoom (/ 500 res-x) :title "Isn't it beautiful?"))
-;(time (image/show (render-plane) :zoom (/ 1000 res-x) :title "Isn't it beautiful?"))
+;(time (image/show (render-plane) :zoom (/ 500 res-x) :title "Isn't it beautiful?"))
 ;(time (image/show (render-sphere) :zoom (/ 500 res-x) :title "Isn't it beautiful?"))
 (time (image/show (render-reflective-sphere) :zoom (/ 500 res-x) :title "Isn't it beautiful?"))
 
 
 (defn render-plane-and-sphere []
   (let [colour-result (v/vec4 [0 0 0 1])
+        ^Vector3 camera-location (v/vec3 [0 1 0])
         ^Vector3 camera-direction (v/vec3 [0 0 1])
         ^Vector3 camera-x (v/vec3 [1 0 0])
         ^Vector3 camera-y (v/vec3 [0 1 0])
@@ -186,6 +210,7 @@
 
 (defn render-plane []
   (let [colour-result (v/vec4 [0 0 0 1])
+        ^Vector3 camera-location (v/vec3 [0 1 0])
         ^Vector3 camera-direction (v/vec3 [0 0 1])
         ^Vector3 camera-x (v/vec3 [1 0 0])
         ^Vector3 camera-y (v/vec3 [0 1 0])
